80ad255b63e90be81f4cdd3ebc6d533a738b476b
Paweł Gronowski <pawel.gronowski@docker.com>
2024-03-07 11:09:14 +0100

    app: list
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- app/list.go
+++ app/list.go
@@ -3,12 +3,234 @@ package app
 import (
 	"context"
 	"fmt"
+	"strings"
+	"unicode/utf8"
 
+	"github.com/containerd/platforms"
 	"github.com/docker/cli/cli/command"
+	imagetypes "github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/pkg/stringid"
+	"github.com/docker/go-units"
+	"github.com/fatih/color"
 )
 
 func List(ctx context.Context, dockerCLI command.Cli, args []string) error {
+	images, err := dockerCLI.Client().ImageList(ctx, imagetypes.ListOptions{
+		All:            true,
+		ContainerCount: true,
+	})
+	if err != nil {
+		return err
+	}
+
+	var view []topImage
+	for _, img := range images {
+		details := imageDetails{
+			ID:   img.ID,
+			Size: units.HumanSizeWithPrecision(float64(img.Size), 3),
+			Used: img.Containers > 0,
+		}
+
+		var children []subImage
+		for _, platform := range img.PlatformImages {
+			sub := subImage{
+				Platform:  platforms.Format(platform.Platform),
+				Available: platform.Available,
+				Details: imageDetails{
+					ID:   platform.ID,
+					Size: units.HumanSizeWithPrecision(float64(platform.ContentSize+platform.UnpackedSize), 3),
+					Used: platform.Containers > 0,
+				},
+			}
+
+			children = append(children, sub)
+		}
+
+		for _, tag := range img.RepoTags {
+			view = append(view, topImage{
+				Name:     tag,
+				Details:  details,
+				Children: children,
+			})
+		}
+	}
+
+	return printImageTree(dockerCLI, view)
+}
+
+type imageDetails struct {
+	ID   string
+	Size string
+	Used bool
+}
+
+type topImage struct {
+	Name     string
+	Details  imageDetails
+	Children []subImage
+}
+
+type subImage struct {
+	Platform  string
+	Available bool
+	Details   imageDetails
+}
+
+func printImageTree(dockerCLI command.Cli, images []topImage) error {
 	out := dockerCLI.Out()
-	fmt.Fprintln(out, "TODO")
+	_, width := out.GetTtySize()
+
+	headers := []header{
+		{Title: "Image", Width: 0, Left: true},
+		{Title: "ID", Width: 12},
+		{Title: "Size", Width: 8},
+		{Title: "Used", Width: 4},
+	}
+
+	const spacing = 3
+	nameWidth := int(width)
+	for _, h := range headers {
+		if h.Width == 0 {
+			continue
+		}
+		nameWidth -= h.Width
+		nameWidth -= spacing
+	}
+
+	maxImageName := len(headers[0].Title)
+	for _, img := range images {
+		if len(img.Name) > maxImageName {
+			maxImageName = len(img.Name)
+		}
+		for _, sub := range img.Children {
+			if len(sub.Platform) > maxImageName {
+				maxImageName = len(sub.Platform)
+			}
+		}
+	}
+
+	if nameWidth > maxImageName+spacing {
+		nameWidth = maxImageName + spacing
+	}
+
+	if nameWidth < 0 {
+		headers = headers[:1]
+		nameWidth = int(width)
+	}
+	headers[0].Width = nameWidth
+
+	headerColor := color.New(color.FgHiWhite).Add(color.Bold)
+
+	// Print headers
+	for i, h := range headers {
+		if i > 0 {
+			_, _ = fmt.Fprint(out, strings.Repeat(" ", spacing))
+		}
+
+		headerColor.Fprint(out, h.PrintC(headerColor, h.Title))
+	}
+
+	_, _ = fmt.Fprintln(out)
+
+	topNameColor := color.New(color.FgBlue).Add(color.Underline).Add(color.Bold)
+	normalColor := color.New(color.FgWhite)
+	normalFaintedColor := color.New(color.FgWhite).Add(color.Faint)
+	greenColor := color.New(color.FgGreen)
+
+	printDetails := func(clr *color.Color, details imageDetails) {
+		truncID := stringid.TruncateID(details.ID)
+		fmt.Fprint(out, headers[1].Print(clr, truncID))
+		fmt.Fprint(out, strings.Repeat(" ", spacing))
+
+		fmt.Fprint(out, headers[2].Print(clr, details.Size))
+		fmt.Fprint(out, strings.Repeat(" ", spacing))
+
+		if details.Used {
+			fmt.Fprint(out, headers[3].Print(greenColor, " ✔ ️"))
+		} else {
+			fmt.Fprint(out, headers[3].Print(clr, " "))
+		}
+	}
+
+	// Print images
+	for _, img := range images {
+		fmt.Fprint(out, headers[0].Print(topNameColor, img.Name))
+		fmt.Fprint(out, strings.Repeat(" ", spacing))
+
+		printDetails(normalColor, img.Details)
+
+		_, _ = fmt.Fprintln(out, "")
+		for idx, sub := range img.Children {
+			clr := normalColor
+			if !sub.Available {
+				clr = normalFaintedColor
+			}
+
+			if idx != len(img.Children)-1 {
+				fmt.Fprint(out, headers[0].Print(clr, "├─ "+sub.Platform))
+			} else {
+				fmt.Fprint(out, headers[0].Print(clr, "└─ "+sub.Platform))
+			}
+
+			fmt.Fprint(out, strings.Repeat(" ", spacing))
+			printDetails(clr, sub.Details)
+
+			fmt.Fprintln(out, "")
+		}
+	}
+
 	return nil
+}
+
+func maybeUint(v int64) *uint {
+	u := uint(v)
+	return &u
+}
+
+type header struct {
+	Title string
+	Width int
+	Left  bool
+}
+
+func truncateRunes(s string, length int) string {
+	runes := []rune(s)
+	if len(runes) > length {
+		return string(runes[:length])
+	}
+	return s
+}
+
+func (h header) Print(color *color.Color, s string) (out string) {
+	if h.Left {
+		return h.PrintL(color, s)
+	}
+	return h.PrintC(color, s)
+}
+
+func (h header) PrintC(color *color.Color, s string) (out string) {
+	ln := utf8.RuneCountInString(s)
+	if h.Left {
+		return h.PrintL(color, s)
+	}
+
+	if ln > int(h.Width) {
+		return color.Sprint(truncateRunes(s, h.Width))
+	}
+
+	fill := int(h.Width) - ln
+
+	l := fill / 2
+	r := fill - l
+
+	return strings.Repeat(" ", l) + color.Sprint(s) + strings.Repeat(" ", r)
+}
+
+func (h header) PrintL(color *color.Color, s string) string {
+	ln := utf8.RuneCountInString(s)
+	if ln > int(h.Width) {
+		return color.Sprint(truncateRunes(s, h.Width))
+	}
+
+	return color.Sprint(s) + strings.Repeat(" ", int(h.Width)-ln)
 }
--- go.mod
+++ go.mod
@@ -3,23 +3,28 @@ module github.com/vvoland/docker-plugin-imgx
 go 1.21
 
 require (
+	github.com/containerd/platforms v0.1.1
 	github.com/docker/cli v25.0.3+incompatible
+	github.com/docker/docker v25.0.3+incompatible
+	github.com/docker/go-units v0.5.0
+	github.com/fatih/color v1.16.0
 	github.com/spf13/cobra v1.8.0
 )
 
+replace github.com/docker/docker => github.com/vvoland/moby v20.10.3-0.20240307102459-945758a3faf9+incompatible
+
 require (
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
-	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/Microsoft/hcsshim v0.10.0 // indirect
 	github.com/beorn7/perks v0.0.0-20150223135152-b965b613227f // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/distribution/reference v0.5.0 // indirect
 	github.com/docker/distribution v2.8.3+incompatible // indirect
-	github.com/docker/docker v25.0.3+incompatible // indirect
 	github.com/docker/docker-credential-helpers v0.8.1 // indirect
 	github.com/docker/go v1.5.1-1.0.20160303222718-d30aec9fd63c // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
 	github.com/docker/go-metrics v0.0.0-20180209012529-399ea8c73916 // indirect
-	github.com/docker/go-units v0.5.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fvbommel/sortorder v1.1.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
@@ -28,8 +33,11 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/gorilla/mux v1.7.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/miekg/pkcs11 v1.0.2 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/sys/sequential v0.5.0 // indirect
 	github.com/moby/term v0.5.0 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
@@ -40,6 +48,7 @@ require (
 	github.com/prometheus/client_model v0.0.0-20171117100541-99fa1f4be8e5 // indirect
 	github.com/prometheus/common v0.0.0-20180110214958-89604d197083 // indirect
 	github.com/prometheus/procfs v0.0.0-20180125133057-cb4147076ac7 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/theupdateframework/notary v0.7.0 // indirect
@@ -50,11 +59,15 @@ require (
 	go.opentelemetry.io/otel/sdk v1.24.0 // indirect
 	go.opentelemetry.io/otel/trace v1.24.0 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/term v0.15.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/tools v0.8.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240102182953-50ed04b92917 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240102182953-50ed04b92917 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 	gotest.tools/v3 v3.5.1 // indirect
 )
